86a1793ff78a35bfa1852ce7e1522244cb2abfb4
2544403004@qq.com <2544403004@qq.com>
2017-06-04 21:46:35 +0800

    home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,26 @@
 import React,{Component} from 'react'
 import BottomTab from './BottomTab'
+import style from '../css/home.css'
 
 
+class HomeTop extends Component{
+	render(){
+		return (
+			<div data-maxwidth className={style.homeTop}>
+				今日推荐
+			</div>
+
+			)
+	}
+}
 
 class Home extends Component{
 	render(){
 		return (
+			<div className={style.home}>
+			<HomeTop />
 			<BottomTab />
-
+			</div>
 			)
 	}
 }
